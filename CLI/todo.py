7102a6f00b1305c5f940c734c70da96a670b46f5
obe <oemer.bektuerk@mediakom-online.de>
2025-10-07 16:00:09 +0200

    base function

--- CLI/todo.py
+++ CLI/todo.py
@@ -51,7 +51,11 @@ def main():
 
     # delete command 
     delete_parser = subparsers.add_parser("delete", help="Lösche eine Aufgabe")
-    delete_parser.add_argument("nummer", type=int, help="Nummer der Aufgabe, die gelöscht werden soll")
+    delete_parser.add_argument("nummer", type=int, help="Nummer der Aufgabe, die gelöscht werden soll") 
+    
+    done_parser = subparsers.add_parser("done", help="Lösche eine Aufgabe")
+    done_parser.add_argument("nummerDone", type=int, help="Nummer der Aufgabe, die fertig ist") 
+
 
     add_parser = subparsers.add_parser("add", help="Füge eine neue Aufgabe hinzu")
     add_parser.add_argument("titel", help="titel")
@@ -92,6 +96,18 @@ def main():
         index = args.nummer - 1  
         # Example: delete the first task (index 0)
         del daten[index]
+
+        with open(datei, "w", encoding="utf-8") as f:
+            json.dump(daten, f, indent=4, ensure_ascii=False)
+
+        print("Deleted successfully!")
+    
+    elif args.command == "done":
+        with open(datei, "r", encoding="utf-8") as f:
+            daten = json.load(f)
+        index = args.nummer - 1  
+        # Example: delete the first task (index 0)
+        print(daten[index])
 
         with open(datei, "w", encoding="utf-8") as f:
             json.dump(daten, f, indent=4, ensure_ascii=False)
